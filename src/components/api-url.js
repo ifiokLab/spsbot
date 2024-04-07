8b327c3310e932c3796a5d2b2242dc99c36fc4d9
ifiokLab <ifiokudoh77@gmail.com>
2024-04-07 13:23:42 +0100

    ll[nm

--- src/components/api-url.js
+++ src/components/api-url.js
@@ -1,4 +1,4 @@
-//const apiUrl = 'https://spsbackend.azurewebsites.net';
-const apiUrl = 'http://127.0.0.1:8000'
+const apiUrl = 'https://spsbackend.azurewebsites.net';
+//const apiUrl = 'http://127.0.0.1:8000'
 
 export default apiUrl;
